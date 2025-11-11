1d56721b05c3ee794c38f95ce3b1effe80c2b579
vankteshdixit <dixitvanktesh08@gmail.com>
2025-11-11 13:44:35 +0530

    __

--- src/main/java/com/vanktesh/project/airBnbApp/advice/GlobalExceptionHandler.java
+++ src/main/java/com/vanktesh/project/airBnbApp/advice/GlobalExceptionHandler.java
@@ -8,8 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.nio.file.AccessDeniedException;
-
+import org.springframework.security.access.AccessDeniedException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
